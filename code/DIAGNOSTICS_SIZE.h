148c0ee422a5ef3e13871948740059e92193de79
Jonathan Lauderdale <jonathan.lauderdale@gmail.com>
2017-04-05 16:03:46 -0400

    Increase values to allow more diagnostics

--- code/DIAGNOSTICS_SIZE.h
+++ code/DIAGNOSTICS_SIZE.h
@@ -21,8 +21,8 @@ C  and "diagSt_size" (statistics-diags) since values here are deliberately small
       INTEGER    nRegions, sizRegMsk, nStats
       INTEGER    diagSt_size
       PARAMETER( ndiagMax = 500 )
-      PARAMETER( numlists = 10, numperlist = 50, numLevels=2*Nr )
-      PARAMETER( numDiags = 1*Nr )
+      PARAMETER( numlists = 25, numperlist = 50, numLevels=3*Nr )
+      PARAMETER( numDiags = 300*Nr )
       PARAMETER( nRegions = 0 , sizRegMsk = 1 , nStats = 4 )
       PARAMETER( diagSt_size = 10*Nr )
 
