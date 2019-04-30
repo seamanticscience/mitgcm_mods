e167ebe3f62d60946e9c66186fe0502a89ba8126
Jonathan Lauderdale <jonathan.lauderdale@gmail.com>
2019-04-30 15:19:53 -0400

    Hack to simulate brine rejection associated with changes in fractional seaice prescribed in pkg/dic

---
+++ salt_plume_mod/SALT_PLUME_OPTIONS.h
@@ -0,0 +1,17 @@
+C $Header: /u/gcmpack/MITgcm/pkg/salt_plume/SALT_PLUME_OPTIONS.h,v 1.3 2011/12/24 01:09:40 jmc Exp $
+C $Name:  $
+
+C CPP options file for salt_plume package
+C Use this file for selecting options within the salt_plume package
+
+#ifndef SALT_PLUME_OPTIONS_H
+#define SALT_PLUME_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_SALT_PLUME
+C Use Salt plume with prescribed sea ice fraction prescribed from the DIC package
+#define SALT_PLUME_DIC
+
+#endif /* ALLOW_SALT_PLUME */
+#endif /* SALT_PLUME_OPTIONS_H */
